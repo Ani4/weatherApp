afb61e1918856694c6eeda9ca4b7dcd232ce73ca
Ani4298 <sharma.ani.4298@gmail.com>
2019-07-31 20:22:24 +0530

    finally after so long i know how to set data from use to server wtihout form feedback :>

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require('express'),
       app = express(),
       bodyParser = require('body-parser'),
       request= require('request'),
-    apiKey ='f3c2d413af279831521211a17aaa7f53';
-
+      apiKey ='f3c2d413af279831521211a17aaa7f53';
 
 app.set("view engine","ejs");
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(express.json({limit:'1mb'}));
 
 
 app.get('/',function(req,res){
@@ -18,10 +17,16 @@ app.get('/',function(req,res){
 });
 
 
+
+
 app.post('/',function (req,res) {
     let city = req.body.city;
+    console.log(req.body);
+    
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
     request(url, function (err, resspons, body) {
+        
+        
         if (err) {
             res.render('index', { weather: null, error: 'Error, please try again' });
         } else {
@@ -33,7 +38,7 @@ app.post('/',function (req,res) {
                 res.render('index', { weather: weather, error: null });
             }
         }
-    });
+    })
 })
 
 app.set('port', process.env.PORT || 3000);
