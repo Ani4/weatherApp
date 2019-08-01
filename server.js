f5ea9e00690889b5378712ef49f2140cd15d1a7a
Ani4298 <sharma.ani.4298@gmail.com>
2019-08-01 09:00:10 +0530

    adding more info

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express'),
 app.set("view engine","ejs");
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.json({limit:'1mb'}));
+// app.use(express.json({limit:'1mb'}));
 
 
 app.get('/',function(req,res){
@@ -18,28 +18,36 @@ app.get('/',function(req,res){
 
 
 
-
-app.post('/',function (req,res) {
+app.post('/', function (req, res) {
     let city = req.body.city;
-    console.log(req.body);
-    
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
-    request(url, function (err, resspons, body) {
-        
-        
-        if (err) {
-            res.render('index', { weather: null, error: 'Error, please try again' });
-        } else {
-            let weather = JSON.parse(body);
-            if (weather.main == undefined) {
+    console.log(city);
+    /* let lon = req.body.lon;
+    let lat = req.body.lat; */
+    /* console.log(req.body); */
+   /*  res.json({
+        status:'OK its work',
+        lat:lat,
+        lon:lon
+    }); */
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    console.log(url);
+        request(url, function (err, resspons, body) {
+            
+            if (err) {
                 res.render('index', { weather: null, error: 'Error, please try again' });
             } else {
-            
-                res.render('index', { weather: weather, error: null });
+                let weather = JSON.parse(body);
+                console.log(weather);
+                
+                if (weather.main == undefined) {
+                    res.render('index', { weather: null, error: 'Error, please try again' });
+                } else {
+                    res.render('index', { weather: weather, error: null });
+                }
             }
-        }
     })
 })
+
 
 app.set('port', process.env.PORT || 3000);
 app.set('ip', process.env.IP || "0.0.0.0");
